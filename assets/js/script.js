ce7640ae4bea82c69b49559df02bcd9d3331a0d6
ebenezer <e.kankam95@hotmail.com>
2021-01-31 22:55:26 -0600

    added a fetch request fro movie trailer. formatted the movie container to fit movies and resized the movie posters to fit page

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,12 +39,13 @@ const fetchApi = () => {
     searchInput.value = '';
 }
 
-function imageSection(movie) {
+// movie posters
+function imageSection(movie, search) {
     const imageContainer = document.createElement('a');
     // add a class
     imageContainer.classList.add('poster');
     // add an attribute
-    imageContainer.setAttribute('href', '#');
+    imageContainer.setAttribute('href', `#`);
     // image path
     const posters = movie.poster_path;
     // create image element
@@ -56,23 +57,54 @@ function imageSection(movie) {
     imageContainer.appendChild(img);
     // append image conatiner to movie container
     movieContainer.appendChild(imageContainer);
+    // displayModal(imageContainer);
+
+    imageContainer.addEventListener('click', () => {
+        let movieTitle = movie.title;
+        getVideo(movieTitle);
+        console.log(movieTitle);
+    });
 
 }
+
+//get video from youtube
+function getVideo(search) {
+    const API_KEY = 'AIzaSyCRTf7HPvvpO0-iXxO9hCUGaA0B19DJBz4';
+    const URL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1';
+
+    const videoContainer = document.querySelector('#my-modal');
+    fetch(`${URL}&q=${search}&key=${API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+
+            // video id
+            const video_Id = data.items[0].id.videoId;
+            console.log(video_Id)
+
+            let output = `
+            <iframe width="400" height="315" src="https://www.youtube.com/embed/${video_Id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            `
+            videoContainer.innerHTML += output;
+        }).catch(error => alert('No trailer found', error));
+
+
+}
+
 
 // TESTING
 let modal = document.getElementById("my-modal");
 let btnOpen = document.getElementById("btn1");
 let btnClose = document.getElementById("close-modal");
 
-btnOpen.onclick = function() {
+btnOpen.onclick = function () {
     modal.style.display = "block";
 }
 
-btnClose.onclick = function() {
+btnClose.onclick = function () {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
